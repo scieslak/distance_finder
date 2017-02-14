cbf725cf43b8613e1b51ebc66d91d276b576adec
scieslak <scieslak@scieslak.com>
2017-02-14 15:21:21 +0000

    Remove setup from calculator_test.rb

--- test/calculator_test.rb
+++ test/calculator_test.rb
@@ -2,40 +2,39 @@ require 'test_helper'
 
 class DistanceFinderTest < Minitest::Test
 
-  def setup
-    @origin = "Slough"
-    @destination = "Basingstoke"
-    @route = DistanceFinder::Calculator.new(@origin, @destination)
-  end
+  @@origin = "Slough"
+  @@destination = "Basingstoke"
+  @@route = DistanceFinder::Calculator.new(@@origin, @@destination)
+
 
   def test_can_output_distance
-    assert @route.distance
+    assert @@route.distance
   end
 
   def test_can_output_duration
-    assert @route.duration
+    assert @@route.duration
   end
 
   def test_can_output_end_address
-    assert @route.end_address.downcase.include?(@destination.downcase)
+    assert @@route.end_address.downcase.include?(@@destination.downcase)
   end
 
   def test_can_output_start_address
-    assert @route.start_address.downcase.include?(@origin.downcase)
+    assert @@route.start_address.downcase.include?(@@origin.downcase)
   end
 
 
   def test_can_build_url
-    url = "https://maps.googleapis.com/maps/api/directions/json?origin=#{@origin}&destination=#{@destination}"
-    assert_equal @route.build_url, url
+    url = "https://maps.googleapis.com/maps/api/directions/json?origin=#{@@origin}&destination=#{@@destination}"
+    assert_equal @@route.build_url, url
   end
 
   def test_can_parse_response
-    refute @route.parse_response["status"].nil?
+    refute @@route.parse_response["status"].nil?
   end
 
   def test_can_get_response
-    assert @route.parse_response
+    assert @@route.get_response
   end
 
 end
